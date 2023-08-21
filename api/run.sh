9c0e62e10ef1a79114afe1e4454f073a4e2cca6a
Maik <142728475+MaikTech@users.noreply.github.com>
2023-08-21 14:11:06 +0300

    Annotate and logically arrange run.sh (#37)

--- api/run.sh
+++ api/run.sh
@@ -13,7 +13,6 @@ then
 fi
 
 # check if curl is installed
-
 if ! [ -x "$(command -v curl)" ]; then
     echo "curl is not installed. Installing..."
     apt-get update --yes --quiet
@@ -28,8 +27,10 @@ else
     echo "$MODEL model found."
 fi
 
+# Build the project
 make build
-# Get the number of available threads on the system
+
+# Get the number of available CPU threads
 n_threads=$(grep -c ^processor /proc/cpuinfo)
 
 # Define context window
@@ -38,18 +39,19 @@ n_ctx=4096
 # Offload everything to CPU
 n_gpu_layers=0
 
-# Define batch size
-n_batch=2096
-# If total RAM is less than 8GB, set batch size to 1024
+# Define batch size based on total RAM
 total_ram=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+n_batch=2096
 if [ $total_ram -lt 8000000 ]; then
     n_batch=1024
 fi
 
+# Display configuration information
 echo "Initializing server with:"
 echo "Batch size: $n_batch"
 echo "Number of CPU threads: $n_threads"
 echo "Number of GPU layers: $n_gpu_layers"
 echo "Context window: $n_ctx"
 
+# Run the server
 exec python3 -m llama_cpp.server --n_ctx $n_ctx --n_threads $n_threads --n_gpu_layers $n_gpu_layers --n_batch $n_batch
