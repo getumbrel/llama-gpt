7791a97bd319bb4188cf7652aeb11e6a292f9be1
Mayank Chhabra <mayankchhabra9@gmail.com>
2023-08-27 16:47:19 +0700

    fix types

--- ui/types/openai.ts
+++ ui/types/openai.ts
@@ -21,13 +21,13 @@ export enum OpenAIModelID {
   LLAMA_13B_CHAT_GGMLV3_Q4_0_MAC = './models/llama-2-13b-chat.bin',
   LLAMA_70B_CHAT_GGMLV3_Q4_0_MAC = './models/llama-2-70b-chat.bin',
   
-  CODE_LLAMA_7B_CHAT_GGMLV3_Q4_0 = '/models/code-llama-7b-chat.gguf',
-  CODE_LLAMA_13B_CHAT_GGMLV3_Q4_0 = '/models/code-llama-13b-chat.gguf',
-  CODE_LLAMA_34B_CHAT_GGMLV3_Q4_0 = '/models/code-llama-34b-chat.gguf',
+  CODE_LLAMA_7B_CHAT_GGUF_Q4_K_M = '/models/code-llama-7b-chat.gguf',
+  CODE_LLAMA_13B_CHAT_GGUF_Q4_K_M = '/models/code-llama-13b-chat.gguf',
+  CODE_LLAMA_34B_CHAT_GGUF_Q4_K_M = '/models/code-llama-34b-chat.gguf',
   
-  CODE_LLAMA_7B_CHAT_GGMLV3_Q4_0_MAC = './models/code-llama-7b-chat.gguf',
-  CODE_LLAMA_13B_CHAT_GGMLV3_Q4_0_MAC = './models/code-llama-13b-chat.gguf',
-  CODE_LLAMA_34B_CHAT_GGMLV3_Q4_0_MAC = './models/code-llama-34b-chat.gguf',
+  CODE_LLAMA_7B_CHAT_GGUF_Q4_K_M_MAC = './models/code-llama-7b-chat.gguf',
+  CODE_LLAMA_13B_CHAT_GGUF_Q4_K_M_MAC = './models/code-llama-13b-chat.gguf',
+  CODE_LLAMA_34B_CHAT_GGUF_Q4_K_M_MAC = './models/code-llama-34b-chat.gguf',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -77,55 +77,55 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     tokenLimit: 4000,
   },
   [OpenAIModelID.LLAMA_7B_CHAT_GGMLV3_Q4_0_MAC]: {
-    id: OpenAIModelID.LLAMA_7B_CHAT_GGMLV3_Q4_0,
+    id: OpenAIModelID.LLAMA_7B_CHAT_GGMLV3_Q4_0_MAC,
     name: 'Llama 2 7B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
   [OpenAIModelID.LLAMA_13B_CHAT_GGMLV3_Q4_0_MAC]: {
-    id: OpenAIModelID.LLAMA_13B_CHAT_GGMLV3_Q4_0,
+    id: OpenAIModelID.LLAMA_13B_CHAT_GGMLV3_Q4_0_MAC,
     name: 'Llama 2 13B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
   [OpenAIModelID.LLAMA_70B_CHAT_GGMLV3_Q4_0_MAC]: {
-    id: OpenAIModelID.LLAMA_70B_CHAT_GGMLV3_Q4_0,
+    id: OpenAIModelID.LLAMA_70B_CHAT_GGMLV3_Q4_0_MAC,
     name: 'Llama 2 70B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.CODE_LLAMA_7B_CHAT_GGMLV3_Q4_0]: {
-    id: OpenAIModelID.CODE_LLAMA_7B_CHAT_GGMLV3_Q4_0,
+  [OpenAIModelID.CODE_LLAMA_7B_CHAT_GGUF_Q4_K_M]: {
+    id: OpenAIModelID.CODE_LLAMA_7B_CHAT_GGUF_Q4_K_M_MAC,
     name: 'Code Llama 7B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.CODE_LLAMA_13B_CHAT_GGMLV3_Q4_0]: {
-    id: OpenAIModelID.CODE_LLAMA_13B_CHAT_GGMLV3_Q4_0,
+  [OpenAIModelID.CODE_LLAMA_13B_CHAT_GGUF_Q4_K_M]: {
+    id: OpenAIModelID.CODE_LLAMA_13B_CHAT_GGUF_Q4_K_M_MAC,
     name: 'Code Llama 13B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.CODE_LLAMA_34B_CHAT_GGMLV3_Q4_0]: {
-    id: OpenAIModelID.CODE_LLAMA_34B_CHAT_GGMLV3_Q4_0,
+  [OpenAIModelID.CODE_LLAMA_34B_CHAT_GGUF_Q4_K_M]: {
+    id: OpenAIModelID.CODE_LLAMA_34B_CHAT_GGUF_Q4_K_M_MAC,
     name: 'Code Llama 34B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.CODE_LLAMA_7B_CHAT_GGMLV3_Q4_0_MAC]: {
-    id: OpenAIModelID.CODE_LLAMA_7B_CHAT_GGMLV3_Q4_0,
+  [OpenAIModelID.CODE_LLAMA_7B_CHAT_GGUF_Q4_K_M_MAC]: {
+    id: OpenAIModelID.CODE_LLAMA_7B_CHAT_GGUF_Q4_K_M_MAC,
     name: 'Code Llama 7B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.CODE_LLAMA_13B_CHAT_GGMLV3_Q4_0_MAC]: {
-    id: OpenAIModelID.CODE_LLAMA_13B_CHAT_GGMLV3_Q4_0,
+  [OpenAIModelID.CODE_LLAMA_13B_CHAT_GGUF_Q4_K_M_MAC]: {
+    id: OpenAIModelID.CODE_LLAMA_13B_CHAT_GGUF_Q4_K_M_MAC,
     name: 'Code Llama 13B',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.CODE_LLAMA_34B_CHAT_GGMLV3_Q4_0_MAC]: {
-    id: OpenAIModelID.CODE_LLAMA_34B_CHAT_GGMLV3_Q4_0,
+  [OpenAIModelID.CODE_LLAMA_34B_CHAT_GGUF_Q4_K_M_MAC]: {
+    id: OpenAIModelID.CODE_LLAMA_34B_CHAT_GGUF_Q4_K_M_MAC,
     name: 'Code Llama 34B',
     maxLength: 12000,
     tokenLimit: 4000,
